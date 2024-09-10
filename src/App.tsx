20e520862dfdf391a219a137109b66c2155643a5
apreswilson <apreswilson@gmail.com>
2024-09-10 14:18:40 -0400

    about added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
 import "./App.scss";
 
-function App() {
+export default function App() {
 
   return (
     <main>
-      <div className="red">
-        <p>Test</p>
-      </div>
+      <section className="top-area">
+        <div className="left-column">
+          <h1>Hello,</h1>
+          <h1>
+            My name is Alex Wilson. I am a passionate Software Developer aimed towards creating
+            responsive and user friendly web applications.
+          </h1>
+        </div>
+        <div className="right-column">
+          <img src="/Personal_Photo.jpg"></img>
+        </div>
+      </section>
+      <section className="about">
+        <h1>About Me</h1>
+        <p>
+          I am currently 22 years old and attend the University of South Florida. I am expected to graduate at the end of 2024 with a bachelor&apos;s degree in information science with a concentration in Web Development. I was born and currently still reside in Clearwater, Florida. My primary language is English; however, I do have a basic understanding of the Spanish language as well.
+        </p>
+        <p>
+          My area of interest revolves around the field of web application development. I enjoy the process and learning about all the different aspects that go into making a fully functional web application for users on the Internet to access and use for its intended purpose. Various components such as stack, data structuring, UI/UX, CI/CD, and the actual process of programming the web application are all crucial components that I work to improve my understanding of regularly.
+        </p>
+        <p>
+          Some of the classes I have taken throughout my program at USF have helped to shape these foundational skills required to venture into the world of web application development. Regarding programming languages, I have taken classes on HTML, CSS, JavaScript, Python, Java, and C++. In my relational database class I learned about the crucial nature of structuring your databases to effectively store information in a database. In my information architecture class, I learned about how to focus on the user experience when it comes to designing and laying out a web application.
+        </p>
+        <p>
+          I have a range of skills in various languages in technologies that allow me the ability to make a web application. Regarding languages, I am proficient in the 3 core languages, that being HTML, CSS, and JavaScript. I do most of my projects in TypeScript though. In addition, I also have proficiency in the CSS pre-processor SASS and Python as well as a basic understanding of Java, C++, and C. When it comes to technology, I utilize GitHub CI/CD for my code, React as the UI library, Node.js as the environment, and MongoDB as the database platform. I am planning on exploring SQL databases such as PostgreSQL. I also have branched out into using technologies such as Astro, Docker for containerizing, and NextJS environment for making full stack React applications. Regarding more general office applications, I have a certification for Microsoft Office Specialist that I took back in high school.
+        </p>
+        <p>
+          I have a various list of work and volunteer experience. My first job I was hired at in 2016 was Publix Supermarkets as a customer service employee. My second job, that initially began as an internship, that I started in 2021 was database management and web development for Westlake Christian School. My third and most recent job I got hired for in 2023 is an invoice specialist for Label IT technologies. I currently still work for Publix and Label IT and worked for Westlake until late 2023. I have also done an internship for Lockheed Martin helping with database management and physical organizing of stencil parts.
+        </p>
+        <p>
+          I encourage those reading this to feel free to look at my portfolio to get more in-depth coverage of things I have completed throughout my journey as a web application developer.
+        </p>
+      </section>
     </main>
   )
 }
-
-export default App
