0793f36d30db538fc029c5b6d2ed5deaa7237d31
apreswilson <apreswilson@gmail.com>
2024-09-23 12:49:31 -0400

    new artifact

--- src/pages/artifacts/artifacts.tsx
+++ src/pages/artifacts/artifacts.tsx
@@ -22,6 +22,32 @@ export default function Artifacts() {
          <p>
             My plans towards progressing towards my career are to continue to work on my software development projects, and research job requirements for a lot of companies so I can tailor what I am learning about to hirable skills. For instance, I will be doing more research into SQL and relational database structuring as my own current experience is document-based NoSQL databases. Additionally, I will be working on my first full stack application to improve my skills and become hirable as a full stack developer instead of just a front-end developer.
          </p>
+         <h1>Project Management Plan</h1>
+         <p>
+            This essay will cover the specific implementation plan of a software-tracking database for a college. This database will be used and maintained by the IT department of the institution. There are six steps that will be the guiding principles for the implementation plan. In start to finish order they are the initiation phase, definition phase, design phase, development phase, implementation phase, and follow-up phase. Basic information about each software must be available such as the type, the developer, different versions, the types of licensing agreements, what departments use it, the computers in the departments on which the software is installed, and the installation dates. The stakeholders in this scenario are the IT department of the college, as well as those who will use it that are outside of the IT department for various reasons.
+         </p>
+         <p>
+            Starting off with the initiation phase, the database will be a Microsoft Excel database. This is because it is cost effective, simple for people to understand, and easily learnable/editable by the members working on it. The team consisting of the development process will be 5 people, me included. The database will have a user interface tab where the audience will be able to query the database depending on the parameter they are looking for. This will make the database user friendly to a wide range of audiences due to Excel being extremely widespread in its understanding. The user interface tab will also include instructions for how to use it for those who are not previously exposed to Excel.
+         </p>
+         <p>
+            Moving onto the definition phase, the excel database will be organized so that each specific software will have its own row in the database, and each column will be the basic information specified in the introduction. This project will have to remain within the bounds of Excel due to XLSX being a very specific format that isn’t accepted by all platforms. In addition, Excel is an extremely widespread software that many people are familiar with, promoting ease of usability. The database itself will be accessible via a shared document to each member of the staff who needs access to it.
+         </p>
+         <p>
+            Moving on to the design phase, there will be three major components. That will be the user interface, the user query, and the actual database. The user interface will send a request based on the query, the query will then be processed in the background, then the specific queried section of the database will be returned to the user interface as a response. Below is a simple diagram of the process.
+         </p>
+         <img src="/Picture1.png"></img>
+         <p>
+            Transitioning into the development phase, all members on the team will be organized, and a timeline will be broken down into phases. All computers that will be worked on for the development process of the database will be selected. Two of the four other members will be assigned to collect the actual data for the database, and the other two will be responsible for programming and designing the actual database. I will act as a supporting role for both groups when deemed necessary and provide oversight.
+         </p>
+         <p>
+            Upon arriving at the implementation phase, the database will be actively being developed, including programmatic features will be written in Visual Basic (VBA). The two collectors will be traveling to each department and actively gathering information, populating and updating database fields with each new list of information they receive. I will be regularly checking in on the progress of both the programmers/designers and the data collectors, making sure that the project is staying on track and heading towards being finished within a reasonable timeframe.
+         </p>
+         <p>
+            Finally, the follow-up phase will include brief training for people who will be regularly using the database for any necessary purposes and making sure that the functionality of the database works clearly, and that error handling is present. We will also allow stakeholders to give us feedback on their experience using the database when they are being trained. This will give us insight as to whether our product came out as desired or not. Last, I will also ask the head of the IT department to contact me if there are any troubles that they run into with the database, as well as providing special training to them to show them how to update the database itself and allow them to determine who needs to be trained for that role.
+         </p>
+         <p>
+            In conclusion, the database implementation plan is divided into six steps, with each section laid out in specific detail as to the process of creating the database. This originates all the way from it just being an idea, to the follow-up process after the database has been developed. Since the database itself is quite simple in its architecture and actual process, it is versatile to more nuanced and specific details that this implementation plan potentially overlooks.
+         </p>
       </main>
    )
 }
